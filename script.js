ef054698548f6e083b0c471038635589e757adc8
steve-the-hermit <stevemacharia309@gmail.com>
2023-06-11 14:26:39 +0300

    added an event listener and other components

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-// Declare a function
-function sayHello(name) {
-    console.log("Hello, " + name + "!");
-  }
+document.addEventListener('DOMContentLoaded', function() {
   
-  // Call the function
-  sayHello("Traveler"); // Output: Hello, Traveler!
-  
+
+  // Smooth scrolling to anchor links
+  const anchorLinks = document.querySelectorAll('a[href^="#"]');
+  anchorLinks.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+
+  // Form validation
+  const form = document.querySelector('#contact-form');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Perform form validation and submission
+    // Add your code here...
+  });
+
+  // Image slider or carousel
+  const slider = document.querySelector('.slider');
+  // Implement your own image slider or use a library like Swiper, Slick, or Owl Carousel
+
+  // Mobile menu toggle
+  const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
+  const mobileMenu = document.querySelector('.mobile-menu');
+  mobileMenuBtn.addEventListener('click', function() {
+    mobileMenu.classList.toggle('active');
+  });
+
+  // Other JavaScript functionalities specific to your project
+  // ...
+
+});
